7a6245d94aba712d528647ae9063f9b96bfd8d1f
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-09-08 23:54:12 +0900

    prop-typesの学習

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // class App extends Component {
 //   render() {
@@ -16,7 +17,7 @@ const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "Hana", age: 25 },
-    { name: "Hamaji" }
+    { name: "Non"}
   ]
 
   return (
@@ -34,8 +35,13 @@ const User = (props) => {
   return <div>I'm {props.name}!!, and {props.age} years old!!</div>
 }
 
-User.defaultProps = {
-  age: 1
+// User.defaultProps = {
+//   age: 1
+// }
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
