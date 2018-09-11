767c635441239d5773605935391b799e774dddb4
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-09-11 13:28:19 +0900

    過去の実装を消してイベント一覧取得処理を作成

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
+import axios from 'axios';
 
-export const increment = () => ({
-  type: INCREMENT
-})
+export const READ_EVENTS = 'READ_EVENTS'
 
-export const decrement = () => ({
-  type: DECREMENT
-})
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+const QUERYSTRING = '?token=token123'
+
+export const readEvents = () => async (dispatch) => {
+  // 非同期処理
+  // Promiseで返る
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+  console.log(response)
+
+  // responseも返す
+  dispatch({ type: READ_EVENTS, response})
+}
