641f2d0081677f05e486e4fc976196ff0b4406dc
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-09-11 20:49:32 +0900

    redux-devtools-extension導入

--- src/index.js
+++ src/index.js
@@ -10,8 +10,11 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+
+const enhancer = process.env.NODE_ENV === 'development' ? composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
