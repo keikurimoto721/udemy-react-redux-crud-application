a60470b7fd1f150da72ff8a7fc63d5931a8ac68f
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2018-09-11 20:19:06 +0900

    イベント新規作成処理の実装

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -11,5 +12,10 @@ export const readEvents = () => async (dispatch) => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
 
   // responseも返す
-  dispatch({ type: READ_EVENTS, response})
+  dispatch({ type: READ_EVENTS, response })
+}
+
+export const postEvent = values => async (dispatch) => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({ type: CREATE_EVENT, response })
 }
